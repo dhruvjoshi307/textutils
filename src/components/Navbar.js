f23d60e6b1cebe12b51b88e580a15f20ca4e1020
dhruvjoshi201 <111216207+dhruvjoshi307@users.noreply.github.com>
2023-05-24 08:01:26 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function Navbar(props) {
   return (
     <nav
-      className={`navbar navbar-expand-lg  navbar-${props.mode}  bg-${props.mode}`}
+      className={`navbar navbar-expand-lg sticky-top navbar-${props.mode}  bg-${props.mode}`}
     >
       <div className="container-fluid">
 
